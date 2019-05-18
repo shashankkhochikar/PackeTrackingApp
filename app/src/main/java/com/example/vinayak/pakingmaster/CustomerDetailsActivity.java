db2316ad09bf4337e138b4263254a7fdcee0cc8b
Shashank Khochikar <shashank.khochikar@gmail.com>
2019-05-18 20:12:19 +0530

    Init Commit

--- app/src/main/java/com/example/vinayak/pakingmaster/CustomerDetailsActivity.java
+++ app/src/main/java/com/example/vinayak/pakingmaster/CustomerDetailsActivity.java
@@ -1,22 +1,38 @@
 package com.example.vinayak.pakingmaster;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.vinayak.pakingmaster.adapters.ItemListAdapter;
+import com.example.vinayak.pakingmaster.baseclasses.BaseActivity;
+import com.example.vinayak.pakingmaster.pojo.GetCustomerListResponse;
+import com.example.vinayak.pakingmaster.utils.Constant;
+import com.example.vinayak.pakingmaster.volley.GsonRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class CustomerDetailsActivity extends AppCompatActivity {
+public class CustomerDetailsActivity extends BaseActivity {
     ListView listView;
     ArrayList<String> items = new ArrayList<>();
     ItemListAdapter adapter;
     Spinner customerNameSipnner;
+    GetCustomerListResponse getCustomerListResponse;
+    ArrayList<String> customer = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +55,11 @@ public class CustomerDetailsActivity extends AppCompatActivity {
 
         setAdapter(items);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.customer_array, android.R.layout.simple_spinner_item);
+       /* ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.customer_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        customerNameSipnner.setAdapter(adapter);
+        customerNameSipnner.setAdapter(adapter);*/
+
+        prepareCustomerList();
 
 
     }
@@ -71,5 +88,55 @@ public class CustomerDetailsActivity extends AppCompatActivity {
                 return true;*/
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void prepareCustomerList() {
+        try {
+            showBusyProgress();
+            JSONObject jo = new JSONObject();
+            jo.put("", "");
+
+            GsonRequest<GetCustomerListResponse> getCustomerListResquest = new GsonRequest<>(Request.Method.POST, Constant.GET_CUSTOMER_LIST, "", GetCustomerListResponse.class,
+                    new Response.Listener<GetCustomerListResponse>() {
+                        @Override
+                        public void onResponse(@NonNull GetCustomerListResponse response) {
+                            hideBusyProgress();
+                            //showToast(""+response.getSuccess().toString());
+                            if (response.getSuccess() == 1) {
+                                getCustomerListResponse = response;
+                                if (getCustomerListResponse.getCustomerList() != null && getCustomerListResponse.getCustomerList().size() > 0) {
+                                    //txtDataNotFound.setVisibility(View.GONE);
+                                    //customerList.setVisibility(View.VISIBLE);
+
+                                    customer = new ArrayList<>(getCustomerListResponse.getCustomerList().size());
+                                    for (int i = 0; i < getCustomerListResponse.getCustomerList().size(); i++) {
+                                        customer.add(getCustomerListResponse.getCustomerList().get(i).getCustname());
+                                    }
+                                    ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                                            (CustomerDetailsActivity.this, android.R.layout.simple_spinner_item,
+                                                    customer);
+                                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                    customerNameSipnner.setAdapter(adapter);
+                                } else {
+                                    showToast("No Customer Found");
+                                }
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    hideBusyProgress();
+                    showToast(error.getMessage());
+                }
+            });
+            getCustomerListResquest.setRetryPolicy(Application.getDefaultRetryPolice());
+            getCustomerListResquest.setShouldCache(false);
+            Application.getInstance().addToRequestQueue(getCustomerListResquest, "getCustomerListResquest");
+        } catch (JSONException e) {
+            hideBusyProgress();
+            Log.e(PakingListActivity.class.getName(),e.getMessage());
+        }
+
+
     }
 }
